c8d08467828af607add564743532ce30a03d8504
Duong Xuan Anh <977-duongx@users.noreply.localhost>
2023-07-05 16:13:31 +0200

    drop file update

--- drop.sql
+++ drop.sql
@@ -1,5 +1,28 @@
 
+-- drop all packages
+drop package insert_package;
 
+-- drop all sequences
+drop sequence jidelni_listek_seq;
+drop sequence objednavka_seq;
+drop sequence dodavatel_seq;
+drop sequence stoly_seq;
+drop sequence pozice_seq;
+drop sequence zamestnanci_seq;
+
+-- drop all triggers
+drop trigger jidelni_listek_trigger;
+drop trigger objednavka_trigger;
+drop trigger stoly_trigger;
+drop trigger pozice_trigger;
+drop trigger zamestnanci_trigger;
+drop trigger dodavatel_trigger;
+
+-- drop all indexes
+drop index idx_jidelni_listek_nazev;
+drop index idx_objednavka_datum;
+drop index idx_zamestnanci_pozice;
+drop index idx_dodavatel_nazev
 
 -- drop all tables
 DROP TABLE objednavka_jidel;
