dcd6f1f4f08fe1d5a5f3db891b20b21cf4c25ee6
Duong Xuan Anh <977-duongx@users.noreply.localhost>
2023-08-07 20:18:53 +0200

    update drop statistics

--- drop_statistics.sql
+++ drop_statistics.sql
@@ -6,9 +6,9 @@ exec dbms_stats.delete_table_stats(USER, 'stoly');
 
 exec dbms_stats.delete_table_stats(USER, 'objednavka');
 
-exec dbms_stats.delete_table_stats(USER, 'pozice');
+exec dbms_stats.delete_table_stats(USER, 'oddeleni');
 
-exec dbms_stats.delete_table_stats(USER, 'denni_trzby');
+exec dbms_stats.delete_table_stats(USER, 'pozice');
 
 exec dbms_stats.delete_table_stats(USER, 'plat_zamestnancu');
 
